5204785515ecf084d8d98dc0bf9a7769a8a92754
Conrado Jorge <67123333+ConradoJorge@users.noreply.github.com>
2021-04-13 21:18:45 -0400

    Added welcome message to Program.cs.

--- FoodOrderCalculatorCLI/Program.cs
+++ FoodOrderCalculatorCLI/Program.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FoodOrderCalculatorCLI
 {
@@ -10,6 +7,12 @@ namespace FoodOrderCalculatorCLI
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("#=================================================#");
+            Console.WriteLine("# Hello and welcome to the Food Order Calculator! #");
+            Console.WriteLine("#       Please select your delivery service.      #");
+            Console.WriteLine("#=================================================#\n");
+
+            UserInterface.MainMenu();
 
         }
     }
